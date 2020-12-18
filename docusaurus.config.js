abcad7cdea8ae429a56faa10236e97d1636e7d27
Rafael del Molino <rafael@2shapes.com>
2020-12-18 16:52:18 +0100

    Config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -88,13 +88,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/rafaeldelm/2shapes.io/edit/master/website/',
+            'https://github.com/rafaeldelm/2shapes.io/edit/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/rafaeldelm/2shapes.io/edit/master/website/blog/',
+            'https://github.com/rafaeldelm/2shapes.io/edit/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
